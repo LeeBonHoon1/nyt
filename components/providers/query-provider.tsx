0611517489b1484dc25d97961d70edd26c85b9e5
이본훈 <80507816+LeeBonHoon1@users.noreply.github.com>
2023-10-24 14:34:55 +0900

    remove .env file

--- components/providers/query-provider.tsx
+++ components/providers/query-provider.tsx
@@ -2,7 +2,7 @@
 
 import { PropsWithChildren } from "react";
 import { QueryClientProvider } from "@tanstack/react-query";
-import getQueryClient from "@/components/providers/getQueryClient";
+import getQueryClient from "@/components/providers/get-query-client";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export const QueryProvider = ({ children }: PropsWithChildren) => {
