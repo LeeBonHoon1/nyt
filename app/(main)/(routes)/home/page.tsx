9bf56e0f1e4da9e26e0de99bbfd7db98279a6f8a
이본훈 <80507816+LeeBonHoon1@users.noreply.github.com>
2023-10-26 14:14:06 +0900

    hydrate react query

--- app/(main)/(routes)/home/page.tsx
+++ app/(main)/(routes)/home/page.tsx
@@ -1,7 +1,16 @@
+import NewsAPIs from "@/common/apis";
 import NewsList from "@/components/news-list";
+import getHydrateQueryClient from "@/hydrate/get-hydrate-query-client";
+import { Hydrate, dehydrate } from "@tanstack/react-query";
+import { PrefetchQuery } from "@/hydrate/prefetch-query";
 
-const HomePage = () => {
-  return <NewsList />;
+const HomePage = async () => {
+  return (
+    /* @ts-expect-error Async Server Component */
+    <PrefetchQuery>
+      <NewsList />
+    </PrefetchQuery>
+  );
 };
 
 export default HomePage;
---
+++ hydrate/prefetch-query.tsx
@@ -0,0 +1,15 @@
+import getHydrateQueryClient from "@/hydrate/get-hydrate-query-client";
+import NewsAPIs from "@/common/apis";
+import { Hydrate, dehydrate } from "@tanstack/react-query";
+import { PropsWithChildren } from "react";
+
+export const PrefetchQuery = async ({ children }: PropsWithChildren) => {
+  const queryClient = getHydrateQueryClient();
+  await queryClient.prefetchInfiniteQuery({
+    queryKey: ["news", "", "", "", 0],
+    queryFn: ({ pageParam = 0 }) => NewsAPIs.getNews("", "", "", pageParam),
+  });
+
+  const dehydrateState = dehydrate(queryClient);
+  return <Hydrate state={dehydrateState}>{children}</Hydrate>;
+};
